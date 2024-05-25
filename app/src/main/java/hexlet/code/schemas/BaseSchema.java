ca982d6bcc60b5ae550932d581f712151de420ea
Admin <shade641114@gmail.com>
2024-05-25 22:29:52 +0300

    Exercise 4-5 2.1

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -9,10 +9,19 @@ import java.util.function.Predicate;
 public abstract class BaseSchema<T> {
     protected Map<String, Predicate<T>> restrictions = new LinkedHashMap<>();
 
+    /**
+     * Checks that all restrictions were respected
+     * @param object to be checked
+     * @return true if all restrictions were respected, otherwise false
+     */
     public boolean isValid(T object) {
         return restrictions.values().stream().allMatch(r -> r.test(object));
     }
 
+    /**
+     * Add nonNull restrictions
+     * @return this object type
+     */
     public BaseSchema<T> required() {
         restrictions.put("required", Objects::nonNull);
         return this;
